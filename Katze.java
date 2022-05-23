9d486b8e60acfb930fc9dd457d156f9a1736bd05
Sabine <Sabine@DESKTOP-T8VPAU9>
2022-05-23 21:50:14 +0200

    Katze zum ersten Mal geändert

--- Katze.java
+++ Katze.java
@@ -1,3 +1,13 @@
-  public class Katze{
-  
-  }
+public class Katze{
+
+	private int anzLeben;
+	private String name;
+	
+	public void setName(String pName) {
+		name = pName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
